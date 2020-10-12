da1d3d725ca0e8027632b94d165756ac3d656ec7
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-10-13 00:40:22 +1100

    Spice things up a little

---
+++ src/Detail.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Detail.css'
+
+export default class Detail extends React.Component {
+    render() {
+        return (
+            <div className="Detail">
+                <header className="Detail-basics">
+                    I'm a software developer and systems integrator. I have experience designing, implementing, maintaining, operating and upgrading software for its entire lifecycle.
+                </header>
+            </div>
+        )
+    }
+}
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Home.css'
+import Detail from './Detail';
 
 export default class Home extends React.Component {
     render() {
         return (
-        <div className="Home">
-            <header className="Home-header">
-                Hi, I'm Luke Kennedy.<br/>
-                I write code for fun and profit.<br/>
-                <a href="https://www.github.com/LLKennedy">Here's my Github!</a>
-            </header>
-        </div>)
+            <div className="Home">
+                <header className="Home-header">
+                    Hi, I'm Luke Kennedy.<br />
+                I write code for fun and profit.<br />
+                    <a href="https://www.github.com/LLKennedy" className="Home-link">Here's my Github!</a>
+                </header>
+                <Detail />
+            </div>
+        )
     }
 }
