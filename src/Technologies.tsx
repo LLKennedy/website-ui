767fbe096544a68c78580ee52735fa0a4f8759df
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-10-13 23:02:51 +1100

    Draw some pink lines in not quite the right spots

--- src/Technologies.tsx
+++ src/Technologies.tsx
@@ -5,6 +5,7 @@ export interface TechnologiesProps {
     horizontalBars?: number;
     verticalBars?: number;
     scrollSpeed?: number;
+    horizonHeightPercent?: number;
 }
 
 interface TechnologiesState {
@@ -14,9 +15,10 @@ interface TechnologiesState {
 
 export default class Technologies extends React.Component<TechnologiesProps, TechnologiesState> {
     public static defaultProps = {
-        horizontalBars: 20,
+        horizontalBars: 40,
         verticalBars: 40,
-        scrollSpeed: 10
+        scrollSpeed: 10,
+        horizonHeightPercent: 50
     }
     // ctx: CanvasRenderingContext2D;
     constructor(props: TechnologiesProps | Readonly<TechnologiesProps>) {
@@ -56,6 +58,8 @@ export default class Technologies extends React.Component<TechnologiesProps, Tec
         if (context === null) {
             return
         }
+        // context.scale(window.devicePixelRatio, window.devicePixelRatio);
+        context.clearRect(0, 0, canvas.width, canvas.height);
         if (context.fillStyle) {
             context.fillStyle = "black";
             context.fillRect(0, 0, canvas.width, canvas.height);
@@ -64,6 +68,21 @@ export default class Technologies extends React.Component<TechnologiesProps, Tec
         context.fillStyle = "red";
         context.arc(50, 50, 50, 0, 2 * Math.PI);
         context.fill();
+        this.drawHorizontalLines(context, canvas.height, canvas.width, this.props.horizontalBars as number, this.props.horizonHeightPercent as number, this.state.step);
         requestAnimationFrame(this.paint);
+    }
+    drawHorizontalLines(ctx: CanvasRenderingContext2D, height: number, width: number, numBars: number, horizonPercent: number, step: number) {
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "magenta";
+        let bottom = height;
+
+        let flatGap = (horizonPercent / 100) * height / numBars;
+        for (let i = 0; i < numBars; i++) {
+            let newH = bottom - (40 * Math.log2(i * flatGap));
+            ctx.beginPath();
+            ctx.moveTo(0, newH);
+            ctx.lineTo(width, newH);
+            ctx.stroke();
+        }
     }
 }
