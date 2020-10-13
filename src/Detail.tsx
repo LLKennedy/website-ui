4bdbd4b7007c42643225d1d5756835b4cb792579
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-10-13 19:35:12 +1100

    Mostly fix the text issue

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -5,9 +5,11 @@ export default class Detail extends React.Component {
     render() {
         return (
             <div className="Detail">
-                <header className="Detail-basics">
-                    I'm a software developer and systems integrator. I have experience designing, implementing, maintaining, operating and upgrading software for its entire lifecycle.
-                </header>
+                <span className="Detail-basics">
+                    <header className="Detail-text">
+                        I'm a software developer and systems integrator. I have experience designing, implementing, maintaining, operating and upgrading software for its entire lifecycle.
+                    </header>
+                </span>
             </div>
         )
     }
--- src/Home.tsx
+++ src/Home.tsx
@@ -8,8 +8,9 @@ export default class Home extends React.Component {
             <div className="Home">
                 <header className="Home-header">
                     Hi, I'm Luke Kennedy.<br />
-                I write code for fun and profit.<br />
+                    I write code for fun and profit.<br />
                     <a href="https://www.github.com/LLKennedy" className="Home-link">Here's my Github!</a>
+                    You can also keep scrolling for more about me.
                 </header>
                 <Detail />
             </div>
